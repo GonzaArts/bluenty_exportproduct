ba2a316b050049f953d5e9dd08506786aeb4a560
GonzaArts <gonzagamer79@gmail.com>
2024-06-12 12:00:32 +0200

    :wrench: chore: Update composer.json and registration.php for Bluenty Export Product extension

--- registration.php
+++ registration.php
@@ -1,8 +1,6 @@
 <?php
-use Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(
-    ComponentRegistrar::MODULE,
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
     'Bluenty_ExportProduct',
     __DIR__
 );
